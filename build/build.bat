96b57916d9fc8f6d25bbbf40d0a95305cabc1ec6
R66 <icppngame@gmail.com>
2018-01-02 22:36:47 +0800

    win下编译调整

--- build/build.bat
+++ build/build.bat
@@ -1,23 +1,23 @@
 cd %~dp0
 
 cd ../spider/main
-go build -o ../../build/bin/spider
+go build -o ../../build/bin/spider.exe
 cd %~dp0
 
 cd ../archer/main
-go build -o ../../build/bin/archer
+go build -o ../../build/bin/archer.exe
 cd %~dp0
 
 cd ../stg/main
-go build -o ../../build/bin/stg
+go build -o ../../build/bin/stg.exe
 cd %~dp0
 
 cd ../krang/main
-go build -o ../../build/bin/krang
+go build -o ../../build/bin/krang.exe
 cd %~dp0
 
 cd ../replay/main
-go build -o ../../build/bin/replay
+go build -o ../../build/bin/replay.exe
 cd %~dp0
 
 pause
