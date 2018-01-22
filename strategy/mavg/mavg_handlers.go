c7e07e83d2950c61908544788fa5b22f4ac5a29c
R66 <icppngame@gmail.com>
2018-01-22 15:30:38 +0800

    调整策略参数

--- strategy/mavg/mavg_handlers.go
+++ strategy/mavg/mavg_handlers.go
@@ -102,26 +102,26 @@ func NewMACDHandler() strategy.FSMHandler {
 		klkind:   protocol.KL1Min,
 		distance: 6,
 		unit:     60,
-		fkrate:   0.25,
-		skrate:   0.18,
+		fkrate:   0.17,
+		skrate:   0.1,
 		dkrate:   0.15,
-		fsdiff:   1.5,
+		fsdiff:   1.2,
 	}
 	p5 := &klParam{
 		klkind:   protocol.KL5Min, // 使用5分钟k线
 		distance: 3,               // 使用N根k线计算斜率
 		unit:     5 * 60,
-		fkrate:   0.2,
-		skrate:   0.15,
+		fkrate:   0.18,
+		skrate:   0.1,
 		dkrate:   0.1,
-		fsdiff:   1.2,
+		fsdiff:   1.0,
 	}
 	p15 := &klParam{
 		klkind:   protocol.KL15Min,
 		distance: 3,
 		unit:     15 * 60,
 		fkrate:   0.18,
-		skrate:   0.25,
+		skrate:   0.15,
 		dkrate:   0.16,
 		fsdiff:   1.2,
 	}
